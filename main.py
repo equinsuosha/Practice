3863083d3ebb1a243a4de396d58559849fa0661e
equinsu osha <>
2022-09-04 23:15:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print ('Hello World')
+
+print('    /|')
+print('   / |')
+print('  /  |')
+print(' /   |')
+print('/____|')
+
+# variable        string 'plan text'
+character_name = 'Jim'
+
+character_age = '50'
+print('There once was a man named '+ character_name + '', '')
+print('he was ' + character_age + ' years old. ')
+
+character_name = 'Mike'
+print('he really the name ' + character_name + '', '')
+print('but didnt like being ' + character_age + '', '')
